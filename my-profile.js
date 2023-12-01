0a1baed83c50a8d3ed520b5d19d53e1382d63914
Nathan Rice <nriceusa@gmail.com>
2023-12-01 16:57:14 -0700

    Begin implementing JavaScript for profile pages of other players.

--- my-profile.js
+++ my-profile.js
@@ -1,3 +1,18 @@
+function loadAchievements() {
+    achievementStates = {  // Temp placeholder for future database values
+        "social-butterfly": false,
+        "master-chef": true,
+        "bookworm": false,
+        "circus-freak": true,
+    }
+
+    for (const achievement in achievementStates) {
+        const achievementElement = document.querySelector("#" + achievement);
+        toggleElement = achievementElement.querySelector(".achievement-toggle");
+        toggleElement.checked = achievementStates[achievement];
+    }
+}
+
 function updateUsername() {
     const username = localStorage.getItem("username");
     const usernameElement = document.querySelector("#username");
@@ -5,10 +20,10 @@ function updateUsername() {
         usernameElement.textContent = username;
     } else {
         usernameElement.textContent = "######";
-    }
+    }    
 }
 
-function updateAchievements() {
+function updateScore() {
     const achievementElements = document.querySelectorAll(".achievement-card");
     let totalAchievements = 0;
     let totalScore = 0;
@@ -20,40 +35,24 @@ function updateAchievements() {
         if (toggleElement.checked) {
             totalAchievements++;
             totalScore += Number(valueElement.textContent);
-        }
-    }
+        }    
+    }    
 
     const scoreElement = document.querySelector("#score");
     const achievementCountElement = document.querySelector("#achievement-count");
     scoreElement.textContent = String(totalScore) + " pts";
     achievementCountElement.textContent = "Achievements: " + totalAchievements;
-}
-
-function loadAchievements() {
-    achievementStates = {  // Temp placeholder for future database values
-        "social-butterfly": false,
-        "master-chef": true,
-        "bookworm": false,
-        "circus-freak": true,
-    }
-
-    for (const achievement in achievementStates) {
-        console.log(achievement);
-        const achievementElement = document.querySelector("#" + achievement);
-        toggleElement = achievementElement.querySelector(".achievement-toggle");
-        toggleElement.checked = achievementStates[achievement];
-    }
-}
+}    
 
 function loadProfile() {
     loadAchievements();
     updateUsername();
-    updateAchievements();
+    updateScore();
 }
 
 loadProfile();
 
 const achievementElements = document.querySelectorAll(".achievement-card");
 for (element of achievementElements) {
-    element.addEventListener("click", updateAchievements);
+    element.addEventListener("click", updateScore);
 }
